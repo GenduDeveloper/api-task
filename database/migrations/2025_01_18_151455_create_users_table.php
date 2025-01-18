4be0cce4ccc09371581340bbb0b6473f4f1bcb2c
Ilya Avkhimenko <avximenko@inbox.ru>
2025-01-18 23:20:18 +0800

    TASK-1: creating migrations for roles and users

--- database/migrations/2025_01_18_151455_create_users_table.php
+++ database/migrations/2025_01_18_151455_create_users_table.php
@@ -13,11 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+
+            $table->foreignId('role_id')
+                ->references('id')
+                ->on('roles')
+                ->cascadeOnDelete();
+
             $table->timestamps();
         });
     }
